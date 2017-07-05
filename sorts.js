d86c6961ebd8e7b5ebe5c71f209f9b2799565a85
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-05 09:30:57 -1000

    old work

--- sorts.js
+++ sorts.js
@@ -1,20 +1,79 @@
 var quicksort = function(array){
 
+ if(array.length < 2){
+    return array;
+  }
+
+  var left = [];
+  var right = [];
+  var pivot = array[0];
+
+  for(var i = 1; i < array.length; i++){
+    if(array[i] < pivot){
+       left.push(array[i]);
+    } else {
+       right.push(array[i]);
+    }
+  }
+
+  return quicksort(left).concat(pivot, quicksort(right));
+
 };
 
 var bubblesort = function(array){
+
+  var i = 0;
+  var j = 0;
+  var temp = 0;
+
+  for(i = 0; i < array.length - 1; i++){
+    for(j = 0; j < array.length - 1 - i; j++){
+      if (array[j+1] < array[j]){
+        temp = array[j+1];
+        array[j+1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
 
 };
 
 var mergesort = function(array){
 
+  if( array.length < 2 ){
+    return array;
+  }
+
+  var midpoint = Math.floor(array.length / 2);
+  var left = [];
+  var right = [];
+
+
+
+
 };
 
 var insertionsort = function(array){
 
+  var temp = 0;
+  for(var i = 1; i < array.length; i++){
+
+    while( (array[tally] >= 0) && (array[tally] > temp) ){
+
+    }
+
+  }
+
 };
 
 var selectionsort = function(array){
+
+  for(var i = 0; i < array.length; i++){
+
+    for(var j = i+1; j < array.length; j++){
+
+    }
+  }
 
 };
 
--- test/javascript-sorts-test.spec.js
+++ test/javascript-sorts-test.spec.js
@@ -2,16 +2,45 @@ var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
-var module =require('../bubble-sort');
+var module = require('../sorts');
 
 describe('JavaScript Sorts', function(){
 
-  beforeEach(function(){
+  var evilArray = [10, 9 , 8 ,7, 6, 5, 4, 3, 2, 1];
 
-    var evilTest = [10, 9 , 8 ,7, 6, 5, 4, 3, 2, 1];
 
+  it('should quicksort an evil array', function(){
+    var quickArray = evilArray;
+    expect(quickArray).to.deep.equal([10,9,8,7,6,5,4,3,2,1]);
+    quickArray = module.quicksort(quickArray);
+    expect(quickArray[(quickArray.length - 1)]).to.equal(10);
+    expect(quickArray).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
   });
 
+  it('should bubblesort an evil array', function(){
+    var bubbleArray = evilArray;
+    expect(bubbleArray).to.deep.equal([10,9,8,7,6,5,4,3,2,1]);
+    module.bubblesort(bubbleArray);
+    expect(bubbleArray[(bubbleArray.length - 1)]).to.equal(10);
+    expect(bubbleArray).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
 
+  it('should mergesort an evil array', function(){
+    var mergArray = module.mergesort(evilArray);
+    expect(mergeArray[(mergeArray.length - 1)]).to.equal(10);
+    expect(mergeArray).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should insertionsort an evil array', function(){
+    var insertionArray = module.insertionsort(evilArray);
+    expect(insertionArray[(insertionArray.length - 1)]).to.equal(10);
+    expect(insertionArray).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should selectionsort an evil array', function(){
+    var selectionArray = module.selectionsort(evilArray);
+    expect(selectionArray[(selectionArray.length - 1)]).to.equal(10);
+    expect(selectionArray).to.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
 
 });
